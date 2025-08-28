6910f3a7cdab2b8e1b225862a244607d28e81770
zael99 <bdinelt@gmail.com>
2025-08-28 18:27:29 +0000

    Update src/build/build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 installGnim() {(
-    apt-get install -y --quiet --no-install-recommends \
-        libgjs-dev \
-        libgtk-3-dev \
+    dnf install -y \
+        gjs-devel \
+        gtk4-devel \
         npm
 
     npm install gnim
@@ -11,19 +11,6 @@ installGnim() {(
 )}
 
 installAstal() {(
-    apt-get install -y --quiet --no-install-recommends \
-        software-properties-common
-
-    # Install Astal
-    apt-get install -y --quiet --no-install-recommends \
-        meson \
-        vala \
-        valadoc \
-        gobject-introspection-devel \
-        wayland-protocols-devel \
-        gtk3-devel gtk-layer-shell-devel \
-        gtk4-devel gtk4-layer-shell-devel
-
     git clone https://github.com/aylur/astal.git /tmp/astal
 
     # Build Astal
@@ -44,30 +31,46 @@ installAstal() {(
 )}
 
 installAGS() {(
-    installGnim
+    #installGnim
+
     installAstal
 
-    apt-get install -y --quiet --no-install-recommends \
-        ninja \
-        golang
+    dnf install -y \
 
     git clone --recurse-submodules https://github.com/aylur/ags /tmp/ags
 
     # Build AGS
     cd /tmp/ags
+    npm install
     meson setup build
     meson install -C build
 
     #Cleanup AGS Build
+    cd ~
     rm -rf /tmp/*
 )}
 
 # Install Terminal Customizations
-apt-get update
-apt-get install -y --quiet --no-install-recommends \
+dnf update
+
+dnf install -y \
     zsh \
     fish \
     gjs
+    gjs-devel \
+    gtk4-devel \
+    meson \
+    npm \
+    gtk3-devel \
+    gtk-layer-shell-devel \
+    gtk4-devel \
+    gtk4-layer-shell-devel \
+    gobject-introspection-devel
+    ninja \
+    golang \
+    vala \
+    valadoc \
+    wayland-protocols-devel
 
 installAGS
 
