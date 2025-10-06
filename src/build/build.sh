d7ceedd737154c5d803ad6c022a22b5a56ec1da2
zael99 <bdinelt@gmail.com>
2025-10-06 11:26:29 -0400

    Update build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # Install Terminal Customizations
-pacman -Syuc git \
-            zsh \
-            fish \
-            npm \
-            sass \
-            yay
+pacman -Syu --noconfirm \
+    git \
+    zsh \
+    fish \
+    npm \
+    sass \
+    yay
 
 yay -Syu libastal-meta aylurs-gtk-shell-git
 
