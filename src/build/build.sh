ef48eda8a665d6e5b710e5f55315352a11c2d7fe
zael99 <bdinelt@gmail.com>
2025-10-06 11:18:51 -0400

    Update build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,84 +1,17 @@
 #!/usr/bin/env bash
 
-installGnim() {(
-    dnf install -y \
-        gjs-devel \
-        gtk4-devel \
-        npm
-
-    npm install gnim
-    npm install typescript esbuild @girs/gtk-4.0 @girs/gjs -D
-)}
-
-installAstal() {(
-    git clone https://github.com/aylur/astal.git /tmp/astal
-
-    # Build Astal
-    cd /tmp/astal/lib/astal/io
-    meson setup build
-    meson install -C build
-
-    cd /tmp/astal/lib/astal/gtk3
-    meson setup build
-    meson install -C build
-
-    cd /tmp/astal/lib/astal/gtk4
-    meson setup build
-    meson install -C build
-
-    #Cleanup Astal Build
-    cd ~
-    rm -rf /tmp/*
-)}
-
-installAGS() {(
-    git clone --recurse-submodules https://github.com/aylur/ags /tmp/ags
-
-    # Build AGS
-    cd /tmp/ags
-    npm install
-    meson setup build
-    meson install -C build
-
-    #Cleanup AGS Build
-    cd ~
-    rm -rf /tmp/*
-)}
-
-installAGSDependencies() {(
-    dnf install -y \
-        sass \
-        gjs \
-        gjs-devel \
-        gtk4-devel \
-        meson \
-        npm \
-        gtk3-devel \
-        gtk-layer-shell-devel \
-        gtk4-devel \
-        gtk4-layer-shell-devel \
-        gobject-introspection-devel \
-        ninja \
-        go \
-        golang \
-        vala \
-        valac \
-        valadoc \
-        wayland-protocols-devel
-)}
-
 # Install Terminal Customizations
-dnf update -y
+pacman -Syu git \
+            zsh \
+            fish \
+            npm \
+            sass \
+            yay
 
-# Install base dependencies
-dnf install -y \
-    git \
-    zsh \
-    fish
-    
-installAGSDependencies
-installAstal
-installAGS
+yay -S libastal-meta
+yay -S aylurs-gtk-shell-git
+
+paccache -r
 
 # Set zsh as the default shell
 # chsh -s $(which zsh)
